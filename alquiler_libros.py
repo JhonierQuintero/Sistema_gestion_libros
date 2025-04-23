c2ec099884953345b76c48dc875356668da6191f
JhonierQuintero <jhonierandresquinteromendoza@gmail.com>
2025-04-22 20:39:59 -0500

    feat: agregando funcion devolver libros

--- alquiler_libros.py
+++ alquiler_libros.py
@@ -1,4 +1,5 @@
-
+import mostrar_libros
+import biblioteca
 
 usuarios={}
 
@@ -19,23 +20,3 @@ def alquilar_libro():
         print(f"nombre: {nombre_libro}, fecha de devolucion: {fecha_devolucion}")
     else:
         print(f"ese libro {nombre_libro} no existe o no esta disponible") 
-
-def devolver_libro():
-    nombre=input("ingrese el nombre del usuario\n").title()
-
-    if nombre in usuarios and usuarios[nombre]:
-        print("-------LIBRO ALQUILADO-------")
-        for i , alquilados in enumerate (usuarios[nombre],1):
-            print(f"{i}. {alquilados['nombre']} - fecha de devolucion {alquilados['fecha de devolucion']} genero: {alquilados['genero']}  ")
-
-        indice=int(input("numero del libro que desea devolver: "))-1
-
-        if 0 <= indice < len(usuarios[nombre]):
-            modelo=usuarios[nombre][indice]["nombre"]
-            biblioteca[modelo]["disponible"]+=1
-            usuarios[nombre].pop(indice)
-            print("el libro fue regresado de manera exitosa.")
-        else :
-            print("numero incorrecto del libro.")
-    else :
-        print("el cliente no tiene libros alquilados.")
