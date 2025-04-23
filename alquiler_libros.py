5007ebf24aba1fbfe98e57a349b60d41742acf2d
JhonierQuintero <jhonierandresquinteromendoza@gmail.com>
2025-04-22 20:29:50 -0500

    feat: agregando archivo biblioteca

--- alquiler_libros.py
+++ alquiler_libros.py
@@ -1,9 +1,4 @@
-biblioteca={
-    "Lord of mistery": {"disponible": 3, "genero": "misterio"},
-    "La vida despues de la muerte": {"disponible": 2, "genero": "aventura"},
-    "Shadow slave": {"disponible": 2, "genero": ",misterio"},
-    "Reverend insanity": {"disponible": 1, "genero": "drama"},
-    }
+
 
 usuarios={}
 
